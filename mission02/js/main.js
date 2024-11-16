2be872611f0b74d2aa907c933c22b737b7c1a571
bohyemian <tomboy.kr@gmail.com>
2024-11-16 11:34:09 +0900

    input 각각의 value를 저장하기 위한 렉시컬 환경 생성

--- mission02/js/main.js
+++ mission02/js/main.js
@@ -1,8 +1,7 @@
-
 const user = {
-  id:'asd@naver.com',
-  pw:'spdlqj123!@'
-}
+  id: 'asd@naver.com',
+  pw: 'spdlqj123!@',
+};
 
 /*
 
@@ -13,24 +12,49 @@ const user = {
 
 */
 
-function emailReg(text){
+function emailReg(text) {
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-  return re.test(String(text).toLowerCase())
+  return re.test(String(text).toLowerCase());
 }
 
-function pwReg(text){
+function pwReg(text) {
   const re = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^*+=-]).{6,16}$/;
   return re.test(String(text).toLowerCase());
 }
 
+function getNode(node, context = document) {
+  if (typeof node === 'string') node = document.querySelector(node);
+  if (node.nodeType === 1) {
+    return node;
+  } else {
+    throw new TypeError(`${node}는 문자열이 아니거나 node가 아닙니다.`);
+  }
+}
 
+function setInputValidation() {
+  let value;
 
+  function validation(callback) {
+    return callback(value);
+  }
 
+  function getValue(e) {
+    value = e.target.value;
+  }
 
+  return { validation, getValue };
+}
 
+const inputEmail = getNode('#userEmail');
+const inputPassword = getNode('#userPassword');
+const submitBtn = getNode('button.btn-login[type=submit]');
+const { validation: checkEmailValid, getValue: getEmailValue } = setInputValidation();
+const { validation: checkPwValid, getValue: getPwValue } = setInputValidation();
 
+inputEmail.addEventListener('input', getEmailValue);
+inputPassword.addEventListener('input', getPwValue);
 
-
-
-
+submitBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+});
